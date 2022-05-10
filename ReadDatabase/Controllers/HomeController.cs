dbcad207b821d5ceb5bfceed4bbf7766bd89da6e
tplogtenberg <tplogtenberg@gmail.com>
2022-05-10 17:39:46 +0200

    Laatste versie met nette code blokjes producten op de homepage

--- ReadDatabase/Controllers/HomeController.cs
+++ ReadDatabase/Controllers/HomeController.cs
@@ -15,10 +15,10 @@ namespace ReadDatabase.Controllers
     }
 
     public IActionResult Index()
-    {    
+    {
       // lijst met producten ophalen
       var products = GetAllProducts();
-      
+
       // de lijst met producten in de html stoppen
       return View(products);
     }
@@ -34,22 +34,51 @@ namespace ReadDatabase.Controllers
       foreach (var row in rows)
       {
         // Voor elke rij maken we nu een product
-        Product p = new Product();
-        p.Naam = row["naam"].ToString();
-        p.Prijs = row["prijs"].ToString();
-        p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
-        p.Id = Convert.ToInt32(row["id"]);
+        Product p = GetProductFromRow(row);
 
         // en dat product voegen we toe aan de lijst met producten
-        products.Add(p);        
+        products.Add(p);
       }
 
       return products;
     }
 
+
+    public Product GetProduct(int id)
+    {
+      // product ophalen uit de database op basis van het id
+      var rows = DatabaseConnector.GetRows($"select * from product where id = {id}");
+            
+      // We krijgen altijd een lijst terug maar er zou altijd één product in moeten
+      // zitten dus we pakken voor het gemak gewoon de eerste
+      Product product = GetProductFromRow(rows[0]);     
+
+      // Als laatste sturen het product uit de lijst terug
+      return product;
+    }
+
+    private Product GetProductFromRow(Dictionary<string, object> row)
+    {
+      Product p = new Product();
+      p.Naam = row["naam"].ToString();
+      p.Prijs = row["prijs"].ToString();
+      p.Beschikbaarheid = Convert.ToInt32(row["beschikbaarheid"]);
+      p.Id = Convert.ToInt32(row["id"]);
+
+      return p;
+    }
+
     public IActionResult Privacy()
     {
       return View();
+    }
+
+    [Route("product/{id}")]
+    public IActionResult ProductDetails(int id)
+    {
+      var product = GetProduct(id);
+
+      return View(product);
     }
 
 
